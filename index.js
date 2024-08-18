9d937069d8dcd8e962f72885cf65bf1a22ad917e
Clyde Forland <122248042+clydehenry3@users.noreply.github.com>
2024-08-17 21:01:16 -0500

    chore: Update FAQ accordion behavior and appearance

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ function toggleDropdown(event) {
     dropdown.classList.remove("active");
     dropdown.querySelector(".plus").style.display = "inline";
     dropdown.querySelector(".minus").style.display = "none";
-    dropdown.querySelector(".answer").style.display = "none";
+    dropdown.querySelector(".answer").style.maxHeight = "0";
+    dropdown.querySelector(".answer").style.opacity = "0";
   });
 
   if (!isActive) {
     currentDropdown.classList.add("active");
     currentDropdown.querySelector(".plus").style.display = "none";
     currentDropdown.querySelector(".minus").style.display = "inline";
-    currentDropdown.querySelector(".answer").style.display = "block";
+    currentDropdown.querySelector(".answer").style.maxHeight = "500px";
+    currentDropdown.querySelector(".answer").style.opacity = "1";
   }
 }
 
